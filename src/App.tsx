4363ad570c68c2f9f01bb89e7ce76788486bb10c
Byron 2016 <bgva2005@yahoo.com>
2022-01-05 17:49:34 -0500

    fix: 1.24.04 enable user in app.tsx
    move useeffect to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import React from "react"
+import { useState, useEffect } from "react"
 // import logo from "./logo.svg"
 import "./App.css"
 import Login from "./pages/Login"
@@ -8,14 +8,45 @@ import Home from "./pages/Home";
 import Register from "./pages/Register";
 
 function App() {
+  // const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  
+
+  useEffect(() => {
+    (
+      async() => {
+        const response =await fetch(
+          'http://localhost:8000/api/user',
+          {
+            headers: {
+              'Content-Type':'application/json'
+            },
+            credentials: 'include',
+          }
+        );
+
+        const content = await response.json();
+        setName(content.name);
+        setEmail(content.email);
+
+      }
+    )();
+  })
+
+    // console.log({
+    //   'lugar':'En App.tsx',
+    //   name,
+    //   email,
+    // })
+    // <Route path="/" element={<Home  name = { name } email = {email} />} />
   return (
     <div className="App">
       <BrowserRouter>
         <Nav />
         <main className="form-signin">
           <Routes>
-          {/* <Route path="/" component={Home} /> */}
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home name = { name } email = {email} />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           </Routes>

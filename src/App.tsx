91c23e0fb2e4f723fe6afe5f1e0d1e28bed056df
Byron 2016 <bgva2005@yahoo.com>
2022-01-05 10:51:10 -0500

    chore(bootstrap): add header template

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,20 @@ import "./App.css"
 function App() {
   return (
     <div className="App">
+
+<nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Home</a>
+    <div>
+      <ul className="navbar-nav me-auto mb-2 mb-md-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
       <main className="form-signin">
   <form>
     <h1 className="h3 mb-3 fw-normal">Please sign in</h1>

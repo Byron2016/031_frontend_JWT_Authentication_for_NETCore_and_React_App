2a64c26093086fdda30d3b1f866314fa5a806736
Byron 2016 <bgva2005@yahoo.com>
2022-01-05 10:36:46 -0500

    chore(bootstrap): modify sign-in template

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,11 @@ function App() {
   <form>
     <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
-    <div className="form-floating">
+
       <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-    </div>
-    <div className="form-floating">
+
       <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-    </div>
+
     <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
   </form>
 </main>

e34b9e47db130586106b9a0eb21ab8b9a4feb6d0
Byron 2016 <bgva2005@yahoo.com>
2022-01-05 18:16:05 -0500

    fix: display logout menu and invoque logout api

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,31 @@
-// import React  from "react";
+import React  from "react";
 import {Link} from "react-router-dom";
 
-const Nav = () => {
-  return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-  <div className="container-fluid">
-    <Link to="/" className="navbar-brand">Home</Link>
-    <div>
+const Nav = (props: {name: string  }) => {
+
+  const handleOnClickLogout = async () => {
+
+    const response = await fetch(
+      'http://localhost:8000/api/logout',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type':'application/json'
+        },
+        credentials:'include'
+      }
+    );
+
+    const content = await response.json();
+    console.log(content);
+
+    
+  }
+
+  let menu;
+
+  if(props.name === ''){
+    menu = (
       <ul className="navbar-nav me-auto mb-2 mb-md-0">
         <li className="nav-item">
           <Link to="/login" className="nav-link active" aria-current="page">Login</Link>
@@ -15,6 +34,22 @@ const Nav = () => {
           <Link to="/register" className="nav-link active" aria-current="page">Register</Link>
         </li>
       </ul>
+    )
+  } else {
+    menu = (
+      <ul className="navbar-nav me-auto mb-2 mb-md-0">
+        <li className="nav-item">
+          <Link to="/login" className="nav-link active" onClick={handleOnClickLogout} aria-current="page">Logout</Link>
+        </li>
+      </ul>
+    )
+  }
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+  <div className="container-fluid">
+    <Link to="/" className="navbar-brand">Home</Link>
+    <div>
+      { menu }
     </div>
   </div>
 </nav>

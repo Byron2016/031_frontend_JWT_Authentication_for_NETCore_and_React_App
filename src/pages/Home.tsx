b3f6dcb039ba1e1a437d0292168081260625a3b4
Byron 2016 <bgva2005@yahoo.com>
2022-01-05 15:41:02 -0500

    fix: 1.19.50 display client name into home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,37 @@
-import React  from "react";
+import  { useState, useEffect }  from "react";
 
 const Home = () => {
+
+  // const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  
+
+  useEffect(() => {
+    (
+      async() => {
+        const response =await fetch(
+          'http://localhost:8000/api/user',
+          {
+            headers: {
+              'Content-Type':'application/json'
+            },
+            credentials: 'include',
+          }
+        );
+
+        const content = await response.json();
+        setName(content.name);
+        setEmail(content.email);
+
+      }
+    )();
+  })
+
   return (
     <div>
-      Home
+      {name ? 'Hi ' + name + ' ( ' + email + ')'  : 'You are not logged in'}
+      {/* Hi {name } you have been registered with your email:  {email} */}
     </div>
   )
 }

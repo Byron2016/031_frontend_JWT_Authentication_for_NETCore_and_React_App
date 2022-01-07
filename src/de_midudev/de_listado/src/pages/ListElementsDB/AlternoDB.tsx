e220fe40894e668c81d3cfb86750bf4b9430cca6
Byron 2016 <bgva2005@yahoo.com>
2022-01-07 15:08:36 -0500

    fix(mdd): add useeffect with fetch V5

--- src/de_midudev/de_listado/src/pages/ListElementsDB/AlternoDB.tsx
+++ src/de_midudev/de_listado/src/pages/ListElementsDB/AlternoDB.tsx
@@ -1,4 +1,4 @@
-import {useState, SyntheticEvent} from "react";
+import {useEffect, useState, SyntheticEvent} from "react";
 import {NoteListDB as Note } from '../../components/Notes/NoteListDB'
 import {NoteTypeDB } from '../../../../custom_typings/index'
 import {getRandomText} from '../../util/randomText'
@@ -11,13 +11,30 @@ export const AlternoDB = () => {
   const [notes, setNotes] = useState([] as NoteTypeDB[]);
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
+  const [loading, setLoading] = useState(true)
 
-  console.log('notesbgva: ', {notes})
+  useEffect(() => {
+    console.log('useEffect')
+    setLoading(true)
+    setTimeout(() => {
+      console.log('ahora!')
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(json => {
+          console.log('seteando las notas')
+          setNotes(json)
+          setLoading(false)
+        })
+
+    },5000)
+  }, [])
+
+  // console.log('notes: ', {notes})
   // debugger; // eslint-disable-line no-debugger
 
   const handleSubmit = (event: SyntheticEvent) => {
     event.preventDefault();
-    console.log('notesbgva: ', {notes})
+    // console.log('notes: ', {notes})
     const noteToAddToState : NoteTypeDB  = {
       userId: notes.length + 1,
       id:  notes.length + 1,
@@ -41,9 +58,14 @@ export const AlternoDB = () => {
     setShowAll(!showAll)
   }
 
+  console.log('render')
+  // debugger // eslint-disable-line no-debugger
+  // if(notes.length === 0) return (<strong>There are not notes to display</strong>)
+
   return (
     <div>
       <h1>Notes</h1>
+      {loading ? <strong>Cargando</strong> : <>
       <button onClick={handleClick}>{showAll ? 'Show important' : 'Show all'}</button>
       <ol>
         {
@@ -56,6 +78,10 @@ export const AlternoDB = () => {
         <input type="text" onChange={handleChange} value = {newNote} />
         <button>Add Note</button>
       </form>
+      </>
+      }
+
+
     </div>
   )
 }
